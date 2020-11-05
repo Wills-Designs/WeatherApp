55f3e8131327b2014942ada9b1ab2dd8da22d2b1
William <william3696@live.com>
2020-11-05 18:13:02 +0000

    add js to pull lat and long from users browser

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-window.addEventListener('load')
+window.addEventListener('load', ()=> {
+  let long;
+  let lat;
+
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude
+      lat = position.coords.latitude
+      console.log(lat)
+      console.log(long)
+    })
+  } else {
+    alert("this sucks")
+  }
+});
